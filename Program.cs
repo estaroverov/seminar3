3f1aaf5b341ae450976c990f59d4800300f4192c
eduard <you@example.com>
2022-10-09 16:27:56 +0300

    Решены все задачи

--- Program.cs
+++ Program.cs
@@ -29,6 +29,7 @@ void isPolyndrom(int number)
     Console.WriteLine(CountCheck == counter ? "Палиндром" : "Не палиндром");
 }
 
+Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
 isPolyndrom(num);
 
@@ -36,15 +37,17 @@ isPolyndrom(num);
 ////////////////находит расстояние между ними в 3D пространстве.
 void PrintArray(int [] arr)
 {
+    Console.WriteLine();
     foreach(int i in arr)
     {
         Console.Write(i + " ");
     }
+    
 }
 int [] FillManualArray(int length)
 {
     int [] arr = new int [length];
-    Console.Write("Введите координаты точки:");
+    Console.WriteLine("Введите координаты точки:");
     for(int i=0; i<length; i++)
         arr[i] = int.Parse(Console.ReadLine());
     return arr;
@@ -75,6 +78,7 @@ double FindDistance3d(int[] a, int[] b)
         sum += Math.Pow((a[i] - b[i]),2);
     }
     double d = Math.Pow(sum,0.5);
+    Console.WriteLine();
     Console.WriteLine($"Расстояние: {d}");
     return d;
 }
@@ -123,8 +127,14 @@ int [] FilterArray(int [] arr, int position)
     } 
     return NewArr;
 }
-Console.Write("Введите размерность массива:");
-int size = int.Parse(Console.ReadLine());
+int defineSizeArr()
+{
+    Console.WriteLine();
+    Console.Write("Введите размерность массива:");
+    return int.Parse(Console.ReadLine());
+}
+
+int size = defineSizeArr();
 Console.Write("Введите индекс для удаления элемента:");
 int index = int.Parse(Console.ReadLine());
 int[] newArr = FillRandomArray(size);
@@ -133,3 +143,29 @@ PrintArray(newArr);
 Console.WriteLine();
 Console.Write("Результирующий массив:");
 PrintArray(FilterArray(newArr,index));
+
+//////////////////////////////Вторая дополнительная задача
+int [] sumArr(int[] arr1, int[] arr2)
+{
+    int size = arr1.Length + arr2.Length;
+    int[] arr = new int [size];
+    int counterArr2 = 0;
+    for(int i = 0; i<size;i++)
+    {
+        if(i<arr1.Length)
+            arr[i] = arr1[i];
+        else
+        {
+            arr[i] = arr2[counterArr2];
+            counterArr2++;
+        }
+    }
+
+    return arr;
+}
+int [] arr1 = FillRandomArray(defineSizeArr());
+int [] arr2 = FillRandomArray(defineSizeArr());
+PrintArray(arr1);
+PrintArray(arr2);
+int [] arr3 = sumArr(arr1, arr2);
+PrintArray(arr3);
