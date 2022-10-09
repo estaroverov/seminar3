f9ccb819bdad22b22b91121371d08abb6c9ab0f5
eduard <you@example.com>
2022-10-09 15:02:20 +0300

    Решена задача поиска расстояния между точками в n-мерном пространстве

--- Program.cs
+++ Program.cs
@@ -4,17 +4,20 @@ void isPolyndrom(int number)
 {
     int counter = 0;
     int num = number;
+
     while(number != 0)
     {
         number /= 10;
         counter++;
     }
+
     int [] arr  = new int [counter];
     for(int i = 0; i<counter; i++)
     {
         arr[i] = num % 10;
         num /= 10;
     }
+
     counter = arr.Length-1;
     int CountCheck = arr.Length/2;
     for (int i = 0; i < CountCheck; i++)
@@ -25,5 +28,60 @@ void isPolyndrom(int number)
     }
     Console.WriteLine(CountCheck == counter ? "Палиндром" : "Не палиндром");
 }
+
 int num = int.Parse(Console.ReadLine());
 isPolyndrom(num);
+
+////////////////Задача 21 принимает на вход координаты двух точек и 
+////////////////находит расстояние между ними в 3D пространстве.
+void PrintArray(int [] arr)
+{
+    foreach(int i in arr)
+    {
+        Console.Write(i + " ");
+    }
+}
+int [] FillManualArray(int length, int[] arr)
+{
+    Console.Write("Введите координаты точки:");
+    for(int i=0; i<length; i++)
+        arr[i] = int.Parse(Console.ReadLine());
+    return arr;
+}
+int [] FillRandomArray(int length, int[] arr, int minVal = -100, int maxVal = 100)
+{
+    for(int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(minVal,maxVal);
+    }
+    return arr;
+}
+int [] DefineDots(int dimention = 3, bool isRandom = true)
+{
+    int [] arr = new int [dimention];
+    if(isRandom == true)
+        return FillRandomArray(dimention,arr);
+    else
+        return FillManualArray(dimention,arr);
+    
+
+}
+
+double FindDistance3d(int[] a, int[] b)
+{
+    double sum = 0;
+    for(int i = 0;i<a.Length;i++)
+    {
+        sum += Math.Pow((a[i] - b[i]),2);
+    }
+    double d = Math.Pow(sum,0.5);
+    Console.WriteLine($"Расстояние: {d}");
+    return d;
+}
+
+int[] firstDot = DefineDots(3,false);
+int[] secondDot = DefineDots(3,false);
+
+PrintArray(firstDot);
+PrintArray(secondDot);
+double d = FindDistance3d(firstDot,secondDot);
