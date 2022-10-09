4b2a0f9b2f346dffb37cb69adecf3d2320a6aead
eduard <you@example.com>
2022-10-09 14:03:53 +0300

    Решена задача на определение палиндрома для разных количеств цифр в числе

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿////Решена задача на определение палиндрома для разных количеств цифр в числе
+
+void isPolyndrom(int number)
+{
+    int counter = 0;
+    int num = number;
+    while(number != 0)
+    {
+        number /= 10;
+        counter++;
+    }
+    int [] arr  = new int [counter];
+    for(int i = 0; i<counter; i++)
+    {
+        arr[i] = num % 10;
+        num /= 10;
+    }
+    counter = arr.Length-1;
+    int CountCheck = arr.Length/2;
+    for (int i = 0; i < CountCheck; i++)
+    {
+            if(arr[i] != arr[counter])
+                break;
+            counter --;
+    }
+    Console.WriteLine(CountCheck == counter ? "Палиндром" : "Не палиндром");
+}
+int num = int.Parse(Console.ReadLine());
+isPolyndrom(num);
